f94e364422aafc9aab0036a75942250ce3f6e176
Caroline-boop <kzueva54@gmail.com>
2023-09-07 22:32:07 +0300

    modified:   "pages/2_\360\237\216\250Data_Insights.py" 	modified:   Home_page.py 	modified:   "pages/2_\360\237\216\250Data_Insights.py"

--- pages/2_🎨Data_Insights.py
+++ pages/2_🎨Data_Insights.py
@@ -1,2 +1,38 @@
 import streamlit as st
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+import seaborn as sns
+
+#from telescopes.main_info import info
+#from utils.plots import plot_galaxies
+#from telescopes.references import image_quality_refs
+
 st.markdown("Developed by __supernova__: https://github.com/supernova-py")
+
+#Differentiate vs Grade
+
+st.header('How does the level of tumor differentiation relate to its grade?')
+
+st.markdown("""
+### Think of tumor behavior as getting grades – like in school. We found:
+
+- Well-behaved tumors mostly get '1st Grade,' less aggressive.
+- 'Moderate' tumors are common in all grades.
+- 'Poorly behaved' tumors often get '3rd Grade,' more aggressive.
+- Some tumors are 'undecided' and can match 'anaplastic grade.'
+- So, nicer-behaved tumors usually get lower grades, while others vary. This helps us understand breast cancer.
+""")
+
+df = pd.read_csv("Breast_Cancer.csv")
+
+# Create a pivot table to summarize the data
+pivot_table = df.pivot_table(index='differentiate', columns='Grade', aggfunc='size', fill_value=0)
+
+# Create a heatmap
+fig, ax = plt.subplots(figsize=(10, 6))
+sns.heatmap(pivot_table, annot=True, fmt='d', cmap='YlGnBu', cbar=True)
+plt.title("Differentiate vs Grade")
+plt.xlabel("Grade")
+plt.ylabel("differentiate")
+st.pyplot(fig)
